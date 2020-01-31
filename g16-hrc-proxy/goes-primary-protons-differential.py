4cbfd872a37a43149d2635bd4b6160529b2a2b0d
Malgosia Sobolewska <msobolewska@cfa.harvard.edu>
2020-01-31 16:13:42 -0500

    Utilized the new json field 'channel'

--- g16-hrc-proxy/goes-primary-protons-differential.py
+++ g16-hrc-proxy/goes-primary-protons-differential.py
@@ -1,11 +1,15 @@
 import numpy as np
 import json
 import requests
-from astropy.table import Table
+from astropy.table import Table, Column
 from Chandra.Time import DateTime
 from astropy.io import ascii
+import re
+from collections import OrderedDict
+
 
 URL = 'https://services.swpc.noaa.gov/json/goes/primary/differential-protons-6-hour.json'
+
 
 def fetch_goes_data():
     """
@@ -13,8 +17,25 @@ def fetch_goes_data():
     """
     json_url = requests.get(URL)
     data = json_url.json()
+    
+    # Remove this for loop when the json files contain
+    # all the keys
+    for dat in data:
+        if 'channel' not in dat.keys():
+            dat.update({'channel': ''})
+        if 'yaw_flip' not in dat.keys():
+            dat.update({'yaw_flip': ''})
+        
     t = Table(data)
     return t
+
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+
+def natural_keys(text):
+    return [atoi(c) for c in re.split('(\d+)',text)]
 
 
 def write_html(dat):
@@ -43,6 +64,16 @@ def write_html(dat):
 <pre>
                             Most Recent GOES 16 Observations
                             Proton Flux particles/cm2-s-ster-MeV
+                     
+                     
+                    P1 = 1.02-1.860 MeV                 P7 = 40.3-73.4 MeV
+                    P2A = 1.9-2.3 MeV                   P8A = 83.7-98.5 MeV
+                    P2B = 2.31-3.340 MeV                P8B = 99.9-118 MeV
+                    P3 = 3.4-6.48 MeV                   P8C = 115-143 MeV
+                    P4 = 5.84-11 MeV                    P9 = 160-242 MeV
+                    P5 = 11.64-23.270 MeV               P10 = 276-404 MeV
+                    P6 = 25.9-38.1 MeV
+
 <br/>
 {'<br/>'.join(dat.pformat(max_lines=-1, max_width=-1))}
 <br/>
@@ -96,7 +127,6 @@ def main():
     for the display on the MTA website. Write an updated MTA website with
     the most recent 2 hours of data.
     """
-
     t = fetch_goes_data()
 
     # Find the most recent 2 hours of data, t_new
@@ -109,42 +139,30 @@ def main():
     # Manipulate energy bands to create a temporaty energy table,
     # energy_tab, in order to sort by the lower energy channel and
     # to convert the energy labels from keV to MeV
-    energy_bands = set(t_new['energy'])
-
-    rows = []
-    for energy_band_keV in energy_bands:
-        tmp = energy_band_keV.split(' ')
-        items = tmp[0].split('-')
-        energy_lower = float(items[0]) / 1000  # convert from keV to MeV
-        energy_upper = float(items[1]) / 1000  # convert from keV to MeV
-        energy_band_MeV = f'{energy_lower}-{energy_upper} MeV'
-        rows.append({'energy_keV': energy_band_keV,
-                     'energy_MeV': energy_band_MeV,
-                     'energy_lower': energy_lower})
-
-    energy_tab = Table(rows)
-    energy_tab.sort('energy_lower')
-
-    # Build a reorganized table, dat, with the most recent 2 hours of data
+    channels = list(set(t_new['channel']))
+    channels.sort(key=natural_keys)
+    
     fluxes = {}
-    fluxes = dict.fromkeys(energy_tab['energy_MeV'], [])
-    for energy_band_keV, energy_band_MeV in zip(energy_tab['energy_keV'],
-                                            energy_tab['energy_MeV']):
-        ok = t_new['energy'] == energy_band_keV
-        fluxes[energy_band_MeV] = np.array(t_new[ok]['flux'] * 1000) # per MeV not keV
+    fluxes = OrderedDict.fromkeys(channels, [])    
+
+    for channel in channels:
+        ok = t_new['channel'] == channel
+        fluxes[channel] = np.array(t_new[ok]['flux'] * 1000) # per MeV not keV
 
     # Get and add a column with time tags
-    ok = t_new['energy'] == t_new['energy'][0]
+    ok = t_new['channel'] == channel
     time_tags = t_new['time_tag'][ok]
+    time_tags = Column([time_tag[:16] for time_tag in time_tags], name='time_tag')
+    
     names = ['time_tags'] + list(fluxes.keys())
     dat = Table(fluxes)
     dat.add_column(time_tags, name='time_tag', index=0)
-
+    
     # Format fluxes
     for colname in dat.colnames:
-        if 'MeV' in colname:        
+        if 'P' in colname:        
             dat[colname].format = '.5f'
-
+            
     # Write html
     string = write_html(dat)
     with open(f"./goes-primary-proton-differential.html", "w") as htmlfile:
